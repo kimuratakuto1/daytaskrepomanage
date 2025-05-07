c2d19f8a9b9da29e5a6caa5289412fd488c16ad3
木村拓と <kimuratakuto@MacBook-Air.local>
2025-05-07 17:11:24 +0900

    ユーザー登録機能(signup)を追加

--- daytaskrepomanage/urls.py
+++ daytaskrepomanage/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('taskmanager.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
