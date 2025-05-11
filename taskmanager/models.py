f48050c7c40cb27c58f37a421db61525e198e282
木村拓と <kimuratakuto@MacBook-Air.local>
2025-05-11 15:42:05 +0900

    ユーザーごとのタスクを管理

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class TaskTemplate(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     is_daily = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -16,6 +18,7 @@ class Task(models.Model):
     completed_at = models.DateTimeField(null=True, blank=True)
     date = models.DateField(default=timezone.now)
     template = models.ForeignKey(TaskTemplate, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} - {self.date}"
@@ -25,6 +28,7 @@ class Report(models.Model):
     content = models.TextField(default='名無しのタイトル')
     is_locked = models.BooleanField(default=False)
     title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.date}の日報"
