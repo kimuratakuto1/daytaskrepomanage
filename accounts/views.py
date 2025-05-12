ffa27e46ac6d03949d85744be1099367e16f8153
木村拓と <kimuratakuto@MacBook-Air.local>
2025-05-12 15:36:41 +0900

    disable signup

--- accounts/views.py
+++ accounts/views.py
@@ -2,22 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from .forms import SignUpForm
+from django.conf import settings
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            # 新しいユーザーを作成
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            # ユーザーをログイン状態にする
-            login(request, user)
-            # ホームページやユーザーのダッシュボードへリダイレクト
-            return redirect('login')  # homeはリダイレクト先のURL名（適宜変更）
-    else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return redirect('login')
 
 def logout_view(requets):
     logout(requets)
